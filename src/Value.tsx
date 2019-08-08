caff1956f881940a301e69f33394b9dc57f237f1
Cezar Andrew Villegas Santarin <andrew.santarin@gmail.com>
2019-08-08 11:57:19 +0800

    feat(value): use methods to the root session

--- src/Value.tsx
+++ src/Value.tsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect, MapStateToProps, MapDispatchToProps } from 'react-redux';
+import NewWindow from 'react-new-window';
 
 import { RootState } from './Root.state';
 
 import { updateValue } from './Value.state';
 
 // Components
+export interface ValueApi {
+  fromWindow: string;
+  createNewWindow: () => void;
+}
+
+export interface ValueApiWindow extends Window, Partial<ValueApi> { 
+}
+
 export type ValueStateProps = {
   value: number;
 };
@@ -20,20 +29,29 @@ export type ValueOwnProps = {
 
 export type ValueState = {
   value: number;
+  windows: {
+    [key: string]: string;
+  }
 };
 
 export type ValueProps = ValueStateProps & ValueDispatchProps & ValueOwnProps;
 
-const createNewWindow = () => {
+const createWindowIdHref = (id: string) => {
   const { host, protocol, pathname } = window.location;
-  const { open } = window;
-  open(`${protocol}//${host}${pathname}?id=${Math.random()}`);
+
+  return `${protocol}//${host}${pathname}?id=${id}`;
 };
 
-export class Value extends Component<ValueProps, ValueState> {
+export class Value extends Component<ValueProps, ValueState> implements ValueApi {
   state: ValueState = {
     value: 0,
+    windows: {},
   };
+
+  componentDidMount() {
+    this.fromWindow = (window as ValueApiWindow).fromWindow || this.fromWindow;
+    this.createNewWindow = (window as ValueApiWindow).createNewWindow || this.createNewWindow;
+  }
 
   incrementValue = (value: number) => value + 1;
   decrementValue = (value: number) => value - 1;
@@ -64,7 +82,28 @@ export class Value extends Component<ValueProps, ValueState> {
     this.props.updateValue(newValue);
   }
 
+  fromWindow = window.location.href;
+
+  createNewWindow = () => {
+    console.log('Opening from', this.fromWindow);
+    const newWindowId = Math.random().toString();
+    const newWindows: ValueState['windows'] = {
+      ...this.state.windows,
+      [newWindowId]: createWindowIdHref(newWindowId),
+    };
+
+    this.setState({
+      windows: newWindows,
+    });
+  }
+
+  handleOpen = (window: Window) => {
+    (window as ValueApiWindow).fromWindow = this.fromWindow;
+    (window as ValueApiWindow).createNewWindow = this.createNewWindow;
+  }
+
   render() {
+    const { windows } = this.state;
     return (
       <div style={{ margin: 8 }}>
         <div><label><code>current url:</code></label> {window.location.href}</div>
@@ -78,7 +117,14 @@ export class Value extends Component<ValueProps, ValueState> {
           <button onClick={this.handleReduxIncrementValueClick}>+</button>
           <button onClick={this.handleReduxDecrementValueClick}>-</button>
         </div>
-        <button onClick={createNewWindow}>[+] Window</button>
+        <button onClick={this.createNewWindow}>[+] Window</button>
+        {Object.keys(windows).map((key) => (
+          <NewWindow
+            key={key}
+            url={windows[key]}
+            onOpen={this.handleOpen}
+          />
+        ))}
       </div>
     )
   }
